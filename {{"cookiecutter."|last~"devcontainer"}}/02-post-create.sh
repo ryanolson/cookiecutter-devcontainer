d3e5811cfd7952de30c6ad4464bf7dc49c11ead3
Ryan Olson <rolson@nvidia.com>
2024-06-19 15:50:10 -0700

    fix rogue double quote

--- {{"cookiecutter."|last~"devcontainer"}}/02-post-create.sh
+++ {{"cookiecutter."|last~"devcontainer"}}/02-post-create.sh
@@ -7,7 +7,7 @@ ZSH_CUSTOM="${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended 
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
-git clone https://github.com/spaceship-prompt/spaceship-prompt.git ${ZSH_CUSTOM}/themes/spaceship-prompt" --depth=1
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git ${ZSH_CUSTOM}/themes/spaceship-prompt --depth=1
 ln -s "${ZSH_CUSTOM}/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 echo "plugins=(git zsh-completions)" >> ~/.zshrc
 echo 'autoload -U promptinit; promptinit' >> ~/.zshrc
