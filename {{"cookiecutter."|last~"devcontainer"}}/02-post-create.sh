e84bade7a99023ed85723536e073e55959e6ff73
Ryan Olson <rolson@nvidia.com>
2024-06-19 15:29:25 -0700

    moving completions to post-create

--- {{"cookiecutter."|last~"devcontainer"}}/02-post-create.sh
+++ {{"cookiecutter."|last~"devcontainer"}}/02-post-create.sh
@@ -6,3 +6,8 @@ echo "Current working directory: $(pwd)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended 
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
 echo "plugins=(git zsh-completions)" >> ~/.zshrc
+
+echo "plugins=(git)" >> ~/.bashrc
+echo "if [ -f /usr/share/bash-completion/bash_completion ]; then" >> ~/.bashrc
+echo "    . /usr/share/bash-completion/bash_completion" >> ~/.bashrc
+echo "fi" >> ~/.bashrc
