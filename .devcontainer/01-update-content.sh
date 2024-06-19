11aabf39e87f8ebb01b1ad948e8c9c3f1cc23b01
Ryan Olson <rolson@nvidia.com>
2024-06-19 12:53:14 -0600

    initial commit

---
+++ .devcontainer/01-update-content.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Running .devcontainer/01-update-content.sh script..."
+echo "Current working directory: $(pwd)"
---
+++ .devcontainer/02-post-create.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+
+echo "Running .devcontainer/02-post-create.sh script..."
+echo "Current working directory: $(pwd)"
