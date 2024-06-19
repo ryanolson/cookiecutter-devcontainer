b642367bc22823da24acdcc1aa5e36ea0ea041f1
Ryan Olson <rolson@nvidia.com>
2024-06-19 14:37:12 -0700

    drop user

--- {{"cookiecutter."|last~"devcontainer"}}/02-post-create.sh
+++ {{"cookiecutter."|last~"devcontainer"}}/02-post-create.sh
@@ -6,4 +6,4 @@ echo "Current working directory: $(pwd)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended 
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
 echo "plugins=(git zsh-completions)" >> ~/.zshrc
-chsh -s $(which zsh) $USER
+chsh -s $(which zsh)
