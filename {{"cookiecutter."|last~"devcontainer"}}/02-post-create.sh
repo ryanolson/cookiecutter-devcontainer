0d4a73e0e1b561972cab57edb4430c6aa53f409b
Ryan Olson <rolson@nvidia.com>
2024-06-19 15:46:13 -0700

    spaceship prompt

--- {{"cookiecutter."|last~"devcontainer"}}/02-post-create.sh
+++ {{"cookiecutter."|last~"devcontainer"}}/02-post-create.sh
@@ -3,9 +3,15 @@
 echo "Running .devcontainer/02-post-create.sh script..."
 echo "Current working directory: $(pwd)"
 
+ZSH_CUSTOM="${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended 
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git ${ZSH_CUSTOM}/themes/spaceship-prompt" --depth=1
+ln -s "${ZSH_CUSTOM}/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 echo "plugins=(git zsh-completions)" >> ~/.zshrc
+echo 'autoload -U promptinit; promptinit' >> ~/.zshrc
+echo 'prompt spaceship' >> ~/.zshrc
 
 echo "plugins=(git)" >> ~/.bashrc
 echo "if [ -f /usr/share/bash-completion/bash_completion ]; then" >> ~/.bashrc
