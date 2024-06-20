9aa95b08e8ce223bd11ee9184197d932e74fc8ad
Ryan Olson <rolson@nvidia.com>
2024-06-19 20:24:06 -0700

    make bira the default zsh theme

--- {{"cookiecutter."|last~"devcontainer"}}/02-post-create.sh
+++ {{"cookiecutter."|last~"devcontainer"}}/02-post-create.sh
@@ -6,9 +6,8 @@ echo "Current working directory: $(pwd)"
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended 
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
-git clone https://github.com/spaceship-prompt/spaceship-prompt.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt --depth=1
-ln -s "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt/spaceship.zsh-theme" "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship.zsh-theme"
 echo "plugins=(git zsh-completions)" >> ~/.zshrc
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="bira"/g' ~/.zshrc
 
 echo "plugins=(git)" >> ~/.bashrc
 echo "if [ -f /usr/share/bash-completion/bash_completion ]; then" >> ~/.bashrc
